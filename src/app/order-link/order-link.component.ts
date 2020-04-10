5c43ef139041836e92f124ade534cb7290bc7cb3
Pankirey <pankirey@gmail.com>
2020-04-10 11:06:09 +0600

    1hour left

--- src/app/order-link/order-link.component.ts
+++ src/app/order-link/order-link.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CategoryProductsService} from '../category-products.service';
+import {ProductService} from '../product.service';
+import {CategoryProducts} from '../category_products';
 
 @Component({
   selector: 'app-order-link',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-link.component.css']
 })
 export class OrderLinkComponent implements OnInit {
+  products: CategoryProducts[];
+  restaurant: any;
+  category;
+  product: CategoryProducts;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private CategoryProductService: CategoryProductsService,
+    private productService: ProductService,
+    private location: Location,
+  ) {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.getProduct();
+    this.getCategory();
+    this.getProductByRestaraunt();
+  }
+
+
+  getProduct() {
+    const id = +this.route.snapshot.paramMap.get('restaraunt_products');
+    this.CategoryProductService.getCategoryProducts(id).subscribe( product => this.products = product);
+    this.productService.getProduct(id).subscribe( product => this.restaurant = product);
+  }
+  getCategory() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getCategory(id).subscribe(category => this.category = category);
+  }
+  getProductByRestaraunt() {
+    const id = +this.route.snapshot.paramMap.get('product_id');
+    this.CategoryProductService.getProductByRestaraunt(id).subscribe(product => this.product = product);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
