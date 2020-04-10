163177c566dee21ddd7bde56e2cdc5dfcb31b63c
Pankirey <pankirey@gmail.com>
2020-04-10 09:55:50 +0600

    with products by restaraunt

---
+++ src/app/category-products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {CategoryProduct} from './category_products-mocks';
+import {CategoryProducts} from './category_products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryProductsService {
+
+  constructor() { }
+  getCategoryProduct(id: number): Observable<any> {
+    return of(CategoryProduct.find(product => product.product_id === id));
+  }
+
+  getCategoryProducts(restId: number): Observable<CategoryProducts[]> {
+    return of(CategoryProduct.filter(product => product.restaraunt_id === restId));
+  }
+
+  getProductsByRestaraunt(id: number): Observable<any> {
+    return of(CategoryProduct.filter(product => product.product_id === id));
+  }
+}
