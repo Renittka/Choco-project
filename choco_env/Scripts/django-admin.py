470a59945445becbf979bbf99a07813c8d8131d2
Anna <renita5701@gmail.com>
2020-04-16 19:26:11 +0600

    back

---
+++ choco_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\WebstormProjects\Choco-project\choco_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
