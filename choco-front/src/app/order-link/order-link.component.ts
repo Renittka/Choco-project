18787ba75373967b002eeb9debe2b6d2770e63bc
Anna <renita5701@gmail.com>
2020-04-16 12:08:01 +0600

    front-update

--- choco-front/src/app/order-link/order-link.component.ts
+++ choco-front/src/app/order-link/order-link.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CategoryProductsService} from '../category-products.service';
+import {ProductService} from '../product.service';
+import {CategoryProducts} from '../category_products';
+import { PRODUCTS } from '../products-mock';
+
 
 @Component({
   selector: 'app-order-link',
@@ -6,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-link.component.css']
 })
 export class OrderLinkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  products = PRODUCTS;
+  category;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) {
+    this.router.events.subscribe((value => {
+      this.getProducts();
+    }));
   }
 
-}
+  ngOnInit() {
+    this.getProducts();
+    this.getCategory();
+  }
+
+    getProducts() {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.productService.getProductsByCategoryId(id).subscribe(products => this.products = products);
+    }
+    getCategory() {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.productService.getCategory(id).subscribe(category => this.category = category);
+    }
+
+    goBack(): void {
+      this.location.back();
+  }
+
+    order(){
+      alert('Your Order is accepted')
+    }
+  }
